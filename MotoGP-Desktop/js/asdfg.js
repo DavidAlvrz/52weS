641d10f894a186cf45f0c9d600f914adad3a03ce
DavidAlvrz <davidalvarezm95@gmail.com>
2025-09-26 17:51:08 +0200

    Prueba

---
+++ MotoGP-Desktop/js/asdfg.js
@@ -0,0 +1,8 @@
+// script.js
+function greet(name) {
+  alert("Hola, " + name + "! Bienvenido a la prueba de ofuscación.");
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  greet("Usuario");
+});

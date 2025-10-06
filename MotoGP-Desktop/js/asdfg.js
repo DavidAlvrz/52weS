bda952a882454c5522614b1394b6b2a7a3be4147
DavidAlvrz <davidalvarezm95@gmail.com>
2025-10-06 20:14:38 +0200

    sesion 2(clase)

--- MotoGP-Desktop/js/asdfg.js
+++
@@ -1,8 +0,0 @@
-// script.js
-function greet(name) {
-  alert("Hola, " + name + "! Bienvenido a la prueba de ofuscación.");
-}
-
-document.addEventListener("DOMContentLoaded", function () {
-  greet("Usuario");
-});
